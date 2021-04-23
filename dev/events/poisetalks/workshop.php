273062cdf760dab522755119e43d9eb388e63169
PhantomRn <sihotanghenokh@gmail.com>
2021-04-23 09:25:20 +0700

    Upload test #6

--- dev/events/poisetalks/workshop.php
+++ dev/events/poisetalks/workshop.php
@@ -24,7 +24,7 @@ try {
   $dbconfirm = 0;
 }
 $mysqli->set_charset("utf8mb4");
-
+/*
 $stmt = $mysqli->prepare("INSERT INTO counting (reg) VALUES (?)");
 $y = 1;
 $stmt->bind_param("i", $y);
@@ -37,11 +37,13 @@ if ($idnum < 10) {
     $xy = "#POISETALKS0$idnum";
 } else {
     $xy = "#POISETALKS$idnum";
-}
+} 
+*/
 
 $stmt = $mysqli->prepare("INSERT INTO workshop (name, email, dob, gender, phone, institution, paymethod) VALUES (?, ?, ?, ?, ?, ?, ?)");
 $stmt->bind_param("sssssss",$_POST['name'], $_POST['email'], $_POST['dob'], $_POST['gender'], $_POST['phone'], $_POST['institution'], $_POST['paymethod']);
 $stmt->execute();
+$idnum = mysqli_insert_id($mysqli);
 $stmt->close();
 
 $uploaddir = '../../fileup/workshop/';
@@ -51,7 +53,7 @@ if (move_uploaded_file($_FILES['payment']['tmp_name'], $uploadfile)) {
 } else {
     $upconfirm = 0;
 }
-
+/*
 $mail = new PHPMailer(true);
 $mail->isSMTP();
 $mail->Host       = 'mail.poiseugm.net';
@@ -67,6 +69,7 @@ $mail->Subject = 'Notifikasi pendaftaran POISETalks';
 $recname = $_POST['name'];
 $mail->Body = "Dear $recname,<br><br>Congratulations, you have sucessfully signed up for our <em>Safety Workshop</em> in Process Engineering Series of Events (POISE) 2021.<br><br>Your invoice number is : $xy <br><br>Through this <em>Safety Workshop</em>, we are hoping for you to have such a marvellous experience and gain awareness regarding the importance of safety.<br>We are very excited and looking forward for your participation.<br><br>Let us know if you have any further questions by contacting:<br>LINE: @poiseugm2021 (POISE UGM)<br>Nabila: 081293934283 (WhatsApp)<br>Natasha: 087898502471 (WhatsApp)<br><br>Best regards,<br>POISE UGM";
 $mail->send();
+*/
 if ($dbconfirm == 1 && $upconfirm == 1) {
     header("Location: https://dev.poiseugm.net/events/poisetalks/success.html");
 } else {
